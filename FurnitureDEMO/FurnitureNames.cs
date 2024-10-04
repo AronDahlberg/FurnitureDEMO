604baf20e65c193251eb347b50860f86d1c323f5
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2024-10-04 22:38:54 +0200

    Added FurnitureNames class

---
+++ FurnitureDEMO/FurnitureNames.cs
@@ -0,0 +1,717 @@
+﻿namespace FurnitureDEMO
+{
+    internal static class FurnitureNames
+    {
+        // This content is not affiliated with, endorsed by, or associated with Inter IKEA Systems B.V. or Ingka Holding B.V.
+        // Content is solely intended for personal use
+        public static readonly List<string> Names =
+        [
+            "ALEX" ,
+            
+            
+            "ANKARSPEL" ,
+            
+            
+            "ANRÄTTA" ,
+            
+            
+            "ASKHOLMEN" ,
+            
+            
+            "BACKIG" ,
+            
+            
+            "BACKSÄLEN" ,
+            
+            
+            "BAGGEBO" ,
+            
+            
+            "BALUNGEN" ,
+            
+            
+            "BARNDRÖM" ,
+            
+            
+            "BEJUBLAD" ,
+            
+            
+            "BELÖNING" ,
+            
+            
+            "BERGMUND" ,
+            
+            
+            "BETYDLIG RÄCKA HUGAD" ,
+            
+            
+            "BILLY" ,
+            
+            
+            "BJÖRKSNÄS" ,
+            
+            
+            "BJÖRKSTA" ,
+            
+            
+            "BLANDA" ,
+            
+            
+            "BLÄDDRARE" ,
+            
+            
+            "BOASTAD" ,
+            
+            
+            "BONDHOLMEN" ,
+            
+            
+            "BRIMNES" ,
+            
+            
+            "BROGRUND" ,
+            
+            
+            "BRUMMIG" ,
+            
+            
+            "BUSUNGE" ,
+            
+            
+            "BÅTSKÄR" ,
+            
+            
+            "DRAGON" ,
+            
+            
+            "DRÖMSLOTT" ,
+            
+            
+            "DRÖNJÖNS" ,
+            
+            
+            "DUKTIG" ,
+            
+            
+            "DYRGRIP" ,
+            
+            
+            "DYTÅG" ,
+            
+            
+            "EDSBRUK" ,
+            
+            
+            "EKEDALEN" ,
+            
+            
+            "EKHOLMA" ,
+            
+            
+            "EKOLN" ,
+            
+            
+            "EKTORP" ,
+            
+            
+            "ESSEBODA" ,
+            
+            
+            "FINMAT" ,
+            
+            
+            "FINNALA" ,
+            
+            
+            "FINSMAKARE" ,
+            
+            
+            "FISKBO" ,
+            
+            
+            "FJÄLLBO" ,
+            
+            
+            "FLISAT" ,
+            
+            
+            "FORNEBY" ,
+            
+            
+            "FRIHETEN" ,
+            
+            
+            "FRYKSÅS" ,
+            
+            
+            "FÄRGKLAR" ,
+            
+            
+            "GALANT" ,
+            
+            
+            "GLADELIG" ,
+            
+            
+            "GLAMBERGET" ,
+            
+            
+            "GODMIDDAG" ,
+            
+            
+            "GRADVIS" ,
+            
+            
+            "GRILLSKÄR" ,
+            
+            
+            "GRILLTIDER" ,
+            
+            
+            "GRÅFJÄLLET" ,
+            
+            
+            "GRÅSALA" ,
+            
+            
+            "GRÖNLID" ,
+            
+            
+            "GULLABERG" ,
+            
+            
+            "GULVIAL" ,
+            
+            
+            "GURSKEN" ,
+            
+            
+            "HAUGA" ,
+            
+            
+            "HAVSTA" ,
+            
+            
+            "HAVSTEN" ,
+            
+            
+            "HEKTAR" ,
+            
+            
+            "HEMKOMST" ,
+            
+            
+            "HEMLAGAD" ,
+            
+            
+            "HEMNES" ,
+            
+            
+            "HOLMSUND" ,
+            
+            
+            "HULTARP" ,
+            
+            
+            "HYLTARP" ,
+            
+            
+            "HÅLLBAR" ,
+            
+            
+            "IDANÄS" ,
+            
+            
+            "IDEALISK" ,
+            
+            
+            "INGATORP" ,
+            
+            
+            "INGOLF" ,
+            
+            
+            "JUTHOLMEN" ,
+            
+            
+            "JÄTTEBO" ,
+            
+            
+            "JÄTTELIK" ,
+            
+            
+            "JÄTTESTA" ,
+            
+            
+            "KAFFEREP" ,
+            
+            
+            "KALLAX" ,
+            
+            
+            "KIVIK" ,
+            
+            
+            "KLAPPA" ,
+            
+            
+            "KLEPPSTAD" ,
+            
+            
+            "KLIPPAN" ,
+            
+            
+            "KLOCKREN" ,
+            
+            
+            "KNALLA" ,
+            
+            
+            "KNOPPÄNG" ,
+            
+            
+            "KNOXHULT" ,
+            
+            
+            "KONCIS" ,
+            
+            
+            "KORKEN" ,
+            
+            
+            "KUGGIS" ,
+            
+            
+            "KULINARISK" ,
+            
+            
+            "KUNGSFORS" ,
+            
+            
+            "KVARNVIK" ,
+            
+            
+            "LACK" ,
+            
+            
+            "LAGAN" ,
+            
+            
+            "LANDSKRONA" ,
+            
+            
+            "LEN" ,
+            
+            
+            "LILLABO" ,
+            
+            
+            "LILLEHEM" ,
+            
+            
+            "LISABO" ,
+            
+            
+            "LIXHULT" ,
+            
+            
+            "LOMMARP" ,
+            
+            
+            "LOMVIKEN" ,
+            
+            
+            "LUSTIGT" ,
+            
+            
+            "LYCKSELE" ,
+            
+            
+            "LÅNESPELARE" ,
+            
+            
+            "LÅNGARYD" ,
+            
+            
+            "LÅNGFJÄLL" ,
+            
+            
+            "LÄCKÖ" ,
+            
+            
+            "LÖRDAGSGODIS" ,
+            
+            
+            "MACKAPÄR" ,
+            
+            
+            "MALM" ,
+            
+            
+            "MAMMUT" ,
+            
+            
+            "MATTRADITION" ,
+            
+            
+            "MATÄLSKARE" ,
+            
+            
+            "MICKE" ,
+            
+            
+            "MIDDAGSMAT" ,
+            
+            
+            "MULIG" ,
+            
+            
+            "MUNSBIT" ,
+            
+            
+            "MÅLA" ,
+            
+            
+            "NEREBY" ,
+            
+            
+            "NIMM" ,
+            
+            
+            "NORDKISA" ,
+            
+            
+            "NORDLI" ,
+            
+            
+            "NORDVIKEN" ,
+            
+            
+            "NORRÅKER" ,
+            
+            
+            "NYHAMN" ,
+            
+            
+            "NYMÅNE" ,
+            
+            
+            "NÄMMARÖ" ,
+            
+            
+            "NÄTBARB" ,
+            
+            
+            "NÄVLINGE" ,
+            
+            
+            "OFANTLIGT" ,
+            
+            
+            "OFTAST" ,
+            
+            
+            "OMBONAD" ,
+            
+            
+            "ORDNING" ,
+            
+            
+            "PATRULL" ,
+            
+            
+            "PEPPRIG" ,
+            
+            
+            "PINNIG" ,
+            
+            
+            "PINNTORP" ,
+            
+            
+            "PJÄTTERYD" ,
+            
+            
+            "POKAL" ,
+            
+            
+            "POÄNG" ,
+            
+            
+            "PÅTÅR" ,
+            
+            
+            "PÄRUP" ,
+            
+            
+            "RANARP" ,
+            
+            
+            "RINNIG" ,
+            
+            
+            "RISATORP" ,
+            
+            
+            "ROCKSJÖN" ,
+            
+            
+            "RÅGODLING" ,
+            
+            
+            "RÅGRUND" ,
+            
+            
+            "RÖDALM" ,
+            
+            
+            "RÖNNINGE" ,
+            
+            
+            "SAMLA" ,
+            
+            
+            "SANDSBERG" ,
+            
+            
+            "SANDSKÄDDA" ,
+            
+            
+            "SEDLIG" ,
+            
+            
+            "SEGERÖN" ,
+            
+            
+            "SILVERHÖJDEN" ,
+            
+            
+            "SILVERSIDA" ,
+            
+            
+            "SIMRISHAMN" ,
+            
+            
+            "SJÖRAPPORT" ,
+            
+            
+            "SKANSNÄS" ,
+            
+            
+            "SKOGSTA" ,
+            
+            
+            "SKRUVBY" ,
+            
+            
+            "SKUBB" ,
+            
+            
+            "SKURUP" ,
+            
+            
+            "SKÅDIS" ,
+            
+            
+            "SLIBB" ,
+            
+            
+            "SLÄKT" ,
+            
+            
+            "SMEDSTORP" ,
+            
+            
+            "SMYCKA" ,
+            
+            
+            "SMÅGÖRA" ,
+            
+            
+            "SNIGLAR" ,
+            
+            
+            "SOLFÅGEL" ,
+            
+            
+            "SOLLERÖN" ,
+            
+            
+            "SONGESAND" ,
+            
+            
+            "SPIKSMED" ,
+            
+            
+            "STORKLINTA" ,
+            
+            
+            "STORSINT" ,
+            
+            
+            "STRANDMON" ,
+            
+            
+            "STRANDÖN" ,
+            
+            
+            "STRIMMIG" ,
+            
+            
+            "STUK" ,
+            
+            
+            "SUNDVIK" ,
+            
+            
+            "SUNNERSTA" ,
+            
+            
+            "SVALKA" ,
+            
+            
+            "SVARTSENAP" ,
+            
+            
+            "SYMFONISK" ,
+            
+            
+            "SÄLLSKAPLIG" ,
+            
+            
+            "SÖDERHAMN" ,
+            
+            
+            "TIDVATTEN" ,
+            
+            
+            "TILLREDA" ,
+            
+            
+            "TISKEN" ,
+            
+            
+            "TJENA" ,
+            
+            
+            "TJOG" ,
+            
+            
+            "TJUSIG" ,
+            
+            
+            "TOFTAN" ,
+            
+            
+            "TOLKNING" ,
+            
+            
+            "TONSTAD" ,
+            
+            
+            "TORNVIKEN" ,
+            
+            
+            "TORPARÖ" ,
+            
+            
+            "TOSTERÖ" ,
+            
+            
+            "TRIXIG" ,
+            
+            
+            "TRÅDFRI" ,
+            
+            
+            "TULLSTA" ,
+            
+            
+            "TYSSEDAL" ,
+            
+            
+            "UDDARP" ,
+            
+            
+            "UPPDATERA" ,
+            
+            
+            "UPPFYLLD" ,
+            
+            
+            "UPPLAGA" ,
+            
+            
+            "UPPSPEL" ,
+            
+            
+            "UPPSTÅ" ,
+            
+            
+            "VADHOLMA" ,
+            
+            
+            "VAPPEBY" ,
+            
+            
+            "VARDAGEN" ,
+            
+            
+            "VARIERA" ,
+            
+            
+            "VIDGA" ,
+            
+            
+            "VIHALS" ,
+            
+            
+            "VILDAPEL" ,
+            
+            
+            "VILTO" ,
+            
+            
+            "VIMLE" ,
+            
+            
+            "VINLIDEN" ,
+            
+            
+            "VISKAFORS" ,
+            
+            
+            "VITTSJÖ" ,
+            
+            
+            "VOXNAN" ,
+            
+            
+            "VRETSTORP" ,
+            
+            
+            "VÅGSJÖN" ,
+            
+            
+            "VÄRLDENS" ,
+            
+            
+            "VÄXTHUS" ,
+            
+            
+            "VÖRDA" ,
+            
+            
+            "ÅKERBÄR" ,
+            
+            
+            "ÅRSTID" ,
+            
+            
+            "ÄLSKVÄRD" ,
+            
+            
+            "ÄPPLARYD" ,
+        ];
+    }
+}
