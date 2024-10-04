cccf3fb51ac7080428b43da770b58c9e39f2c007
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2024-10-04 22:38:44 +0200

    Added Lamp class

---
+++ FurnitureDEMO/Lamp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FurnitureDEMO
+{
+    internal class Lamp : Furniture, IComparable<Lamp>
+    {
+        public double Current {  get; set; }
+
+        public int CompareTo(Lamp? other)
+        {
+            return other != null
+                    ? Current.CompareTo(other.Current)
+                    : 1;
+        }
+        public override string ToString()
+        {
+            return $"Lampan {Name} drar {Current} amp";
+        }
+    }
+}
