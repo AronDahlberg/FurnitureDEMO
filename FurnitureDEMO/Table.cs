d5b3d97603906971c1b984523422fb0d1e57985e
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2024-10-04 22:38:35 +0200

    Added Table class

---
+++ FurnitureDEMO/Table.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FurnitureDEMO
+{
+    internal class Table:Furniture, IComparable<Table>
+    {
+        public double AreaOfTable { get; set; }
+
+        public int CompareTo(Table? other)
+        {
+            return other != null
+                    ? AreaOfTable.CompareTo(other.AreaOfTable)
+                    : 1;
+        }
+        public override string ToString()
+        {
+            return $"Bordet {Name} har ytan {AreaOfTable}";
+        }
+    }
+}
